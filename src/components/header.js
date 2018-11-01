b9c915be3f4b06f2edb4a0b06dcf3aa6706bf01e
Magus <abyssmal@mail.ru>
2018-11-01 02:14:54 +0000

    Update src/components/header.js
    Modified header with css-in-js

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,47 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { css } from 'emotion'
+
+
+const div1 = css ({
+  backgroundColor : 'skyblue'
+})
+
+const div2 = css ({
+  margin: '0 auto',
+  maxWidth: 960,
+  padding: '1.45rem 1.0875rem'
+})
+
+const H1 = css ({
+  margin: '0'
+})
+
+const LinkStyle = css ({
+  color: 'white',
+  textDecoration: 'none',
+  textShadow : 'none'
+})
 
 const Header = ({ siteTitle }) => (
+  <div
+    className={div1}
+  >
+    <div
+      className={div2}>
+      <h1 className={H1}>
+        <Link to="/" className = {LinkStyle}>
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+export default Header
+
+
+/*const Header = ({ siteTitle }) => (
   <div
     style={{
       background: 'rebeccapurple',
@@ -30,4 +70,4 @@ const Header = ({ siteTitle }) => (
   </div>
 )
 
-export default Header
+export default Header*/
