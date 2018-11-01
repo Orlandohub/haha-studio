ad48b1532b007b8220aae1c6e8144a91102dcfc9
Magus <abyssmal@mail.ru>
2018-11-01 02:13:50 +0000

    Update src/utils/typography.js
    creation of typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from 'typography'
+import oceanBeachTheme from 'typography-theme-ocean-beach'
+
+const typography = new Typography(oceanBeachTheme)
+
+export default typography
+
+
+/*import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '25px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Georgia', 'serif'],
+  // See below for the full list of options.
+})
+export default typography*/
