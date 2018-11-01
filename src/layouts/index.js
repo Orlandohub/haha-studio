508322a2e63f8f3705954fdc5bf54209bfd73a5d
Magus <abyssmal@mail.ru>
2018-11-01 02:29:37 +0000

    Update src/layouts/index.js
    Replaced the code from the other working project as I was unable to get this one going , but it was necessary to see Header changes take place.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/header'
 import './index.css'
+import { StaticQuery, graphql } from 'gatsby'
 
-const Layout = ({ children, data }) => (
+import { css } from 'react-emotion'
+
+const div3 = css ({
+  margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0
+})
+
+/*const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
@@ -15,14 +24,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className = {div3} >
       {children()}
     </div>
   </div>
@@ -42,4 +44,43 @@ export const query = graphql`
       }
     }
   }
-`
+`*/
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'illya test site' },
+            { name: 'keywords', content: 'test, site, illya, gatsby, react' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          className = {div3}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
+
