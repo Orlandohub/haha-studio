471cc7db50cffad25fa1e5c9618073e7c4942450
Magus <abyssmal@mail.ru>
2018-11-01 02:16:59 +0000

    Update src/pages/404.js
    did I do this?

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import Header from '../components/header'
 const NotFoundPage = () => (
   <div>
+    <Header />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
