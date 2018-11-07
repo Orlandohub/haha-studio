03b21fb73ba9c920606e79fc7727586148767772
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-11-07 21:22:53 +0100

    confirm

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Router } from "react-router-dom";
-import { Provider } from "unstated";
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { Provider } from 'unstated'
 
 export const replaceRouterComponent = ({ history }) => {
   const ConnectedRouterWrapper = ({ children }) => (
     <Provider>
       <Router history={history}>{children}</Router>
     </Provider>
-  );
+  )
 
-  return ConnectedRouterWrapper;
-};
+  return ConnectedRouterWrapper
+}
