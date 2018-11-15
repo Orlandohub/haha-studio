aec0b9678b383aa227a1eae9e262595270a5b4a3
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-11-15 16:23:03 +0100

    netlify added

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,6 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-emotion',
+    'gatsby-plugin-netlify', // make sure to put last in the array
   ],
 }
