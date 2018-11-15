7f1946be71ec856171244b25bc2d1e88d11bb983
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-11-15 17:01:39 +0100

    trial

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Provider } from 'unstated'
-
-export const wrapRootElement = ({ element }) => {
-  sessionStorage.setItem('pathOnStart', window.location.pathname)
-  const ConnectedRootElement = <Provider>{element}</Provider>
-  return ConnectedRootElement
-}
