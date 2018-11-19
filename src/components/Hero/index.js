03b1df282eb5242decf3a9143dc38baab60f339b
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-11-19 11:51:37 +0100

    hero loading

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,69 +3,58 @@ import PropTypes from 'prop-types'
 import { css } from 'react-emotion'
 import arrow from '../../images/arrow.png'
 import { styles } from './styles'
-import { Subscribe } from 'unstated'
-import CurrentPageContainer from '../../state/CurrentPageContainer'
 import { Grid, Row, Col } from 'react-bootstrap'
 
 import Img from 'gatsby-image'
 
-const Hero = ({ data }) => {
-  return (
-    <Subscribe to={[CurrentPageContainer]}>
-      {currentPage => (
-        <div data-cy="hero">
-          {currentPage.state.showHero ? (
-            <div className={css(styles.HeroImageWrapper)}>
-              {currentPage.state.logoLoaderIsVisible ? (
-                <div className={css(styles.HeroLogoLoader)}>
-                  <Grid bsClass={`container ${css(styles.HeroLogoContainer)}`}>
-                    <Row>
-                      <Col xs={12}>
-                        <Img
-                          onLoad={() => currentPage.hideLogoLoader()}
-                          fluid={data.fileName.childImageSharp.fluid}
-                        />
-                      </Col>
-                    </Row>
-                  </Grid>
-                </div>
-              ) : (
-                <div className={css(styles.HiddenHeroLogoLoader)}>
-                  <Grid bsClass={`container ${css(styles.HeroLogoContainer)}`}>
-                    <Row>
-                      <Col xs={12}>
-                        <Img fluid={data.fileName.childImageSharp.fluid} />
-                      </Col>
-                    </Row>
-                  </Grid>
-                </div>
-              )}
-
-              <div className={css(styles.ArrowContainer)}>
-                <img
-                  data-cy="arrow"
-                  className={css(styles.Img)}
-                  src={arrow}
-                  alt="arrow"
-                  onClick={() => currentPage.toggleHero()}
-                />
-              </div>
-            </div>
-          ) : (
-            <div className={css(styles.HideHero)}>
-              <div className={css(styles.HideArrow)}>
-                <img
-                  className={css(styles.HideArrow)}
-                  src={arrow}
-                  alt="arrow"
-                />
-              </div>
-            </div>
-          )}
+class Hero extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      logoLoaderIsVisible: true
+    }
+  }
+  componentDidMount() {
+    setTimeout(
+      function() {
+        this.setState({
+          logoLoaderIsVisible: false
+        })
+      }
+        .bind(this),
+      3000
+    )
+  }
+  render() {
+    const { data } = this.props
+    const { logoLoaderIsVisible } = this.state
+    return (
+      <div data-cy="hero">
+        <div className={css(styles.HeroImageWrapper)}>
+          <div className={logoLoaderIsVisible ? css(styles.HeroLogoLoader) : css(styles.HiddenHeroLogoLoader)}>
+            <Grid bsClass={`container ${css(styles.HeroLogoContainer)}`}>
+              <Row>
+                <Col xs={12}>
+                  <Img
+                    fluid={data.fileName.childImageSharp.fluid}
+                  />
+                </Col>
+              </Row>
+            </Grid>
+          </div>
+          <div className={css(styles.ArrowContainer)}>
+            <img
+              data-cy="arrow"
+              className={css(styles.Img)}
+              src={arrow}
+              alt="arrow"
+            />
+          </div>
         </div>
-      )}
-    </Subscribe>
-  )
+      </div>
+    )
+  }
 }
 
 Hero.propTypes = {
