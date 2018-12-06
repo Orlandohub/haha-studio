d0ff3b99f342457ac55eedd5e7434dcb58a81042
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-12-06 20:37:33 +0100

    posts working

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,34 @@ module.exports = {
         name: 'pages',
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            }
+          }
+        ],
+      },
+    },
     'gatsby-v2-plugin-page-transitions',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
