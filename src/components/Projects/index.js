9c6ea7b9a4136ffbdebc76880226028544456d48
Alphascythian <42778608+Alphascythian@users.noreply.github.com>
2018-12-07 14:43:20 +0000

    Fixes#53
    Arrow to Top placed  in projects component.
    
    Appears after 2500px down.
    
    Some bugs fixed

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,37 @@ import { css } from 'emotion'
 import image_one from '../../images/D_homepage_image_01.jpg'
 import { styles } from './styles'
 
+import ScrollTop from 'react-scrolltop-button'
+import styled from 'react-emotion'
+import facepaint from 'facepaint'
+import arrowToTop from '../../images/arrow_go_to_top.svg'
+const breakpoints = [576, 768, 1024, 1200]
+
+const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
+
+const StyledScrollTop = styled(ScrollTop)`
+  position: fixed;
+  ${mq({ right: ['16px', '16px', '3%', '4.5%'] })};
+  ${mq({ display: ['none', 'none', 'block', 'block', 'block'] })};
+  bottom: 5%;
+  padding: 15px;
+  border: none;
+  border-radius: 50px;
+  background-image: url(${arrowToTop});
+  background-position: center;
+  background-color: transparent;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  transition: all 0.1s;
+  z-index: 0;
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    filter: gray saturate(0%) brightness(70%) contrast(1000%);
+  }
+`
+
 class Projects extends Component {
   render() {
     return (
@@ -13,7 +44,18 @@ class Projects extends Component {
         <div className={css(styles.imageWrapper)}>
           <img src={image_one} />
         </div>
-
+        <div className={css(styles.projectTitle)}>
+          <p>Spin Lamp, 2018</p>
+        </div>
+        <div className={css(styles.imageWrapper)}>
+          <img src={image_one} />
+        </div>
+        <div className={css(styles.projectTitle)}>
+          <p>Spin Lamp, 2018</p>
+        </div>
+        <div className={css(styles.imageWrapper)}>
+          <img src={image_one} />
+        </div>
         <div className={css(styles.projectTitle)}>
           <p>Liberty Lamp, 2018</p>
         </div>
@@ -32,6 +74,13 @@ class Projects extends Component {
         <div className={css(styles.imageWrapper)}>
           <img src={image_one} />
         </div>
+        <StyledScrollTop
+          text=""
+          distance={2500}
+          breakpoint={768}
+          speed={500}
+          target={0}
+        />
       </div>
     )
   }
--- src/pages/selected.js
+++ src/pages/selected.js
@@ -2,36 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from '../layouts'
 import Projects from '../components/Projects'
-import arrowToTop from '../images/arrow_go_to_top.svg'
-import ScrollTop from 'react-scrolltop-button'
-
-import styled from 'react-emotion'
-import facepaint from 'facepaint'
-
-const breakpoints = [576, 768, 1024, 1200]
-
-const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
-
-const StyledScrollTop = styled(ScrollTop)`
-  position: fixed;
-  ${mq({ right: ['16px', '16px', '40px', '70px', '80px'] })};
-  bottom: 5%;
-  padding: 15px;
-  border: none;
-  border-radius: 50px;
-  background-image: url(${arrowToTop});
-  background-position: center;
-  background-color: transparent;
-  background-repeat: no-repeat;
-  cursor: pointer;
-  transition: all 0.5s;
-  &:focus {
-    outline: none;
-  }
-  &:hover {
-    filter: gray saturate(0%) brightness(70%) contrast(1000%);
-  }
-`
 
 class Selected extends React.Component {
   render() {
@@ -39,13 +9,6 @@ class Selected extends React.Component {
     return (
       <Layout location={location}>
         <Projects />
-        <StyledScrollTop
-          text=""
-          distance={100}
-          breakpoint={100}
-          speed={500}
-          target={30}
-        />
       </Layout>
     )
   }
