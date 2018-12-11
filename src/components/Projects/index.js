ed37cafdfcf53f0fce1b6116b4e0dbdaf5d2e130
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-12-11 11:50:58 +0100

    projects became stateless function

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,97 +1,69 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { css } from 'emotion'
 import image_one from '../../images/D_homepage_image_01.jpg'
 import { styles, StyledScrollTop } from './styles'
 
-class Projects extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { distanceTop: 2500 }
-
-    this.handleResize = this.handleResize.bind(this)
-  }
-
-  handleResize() {
-    window.innerWidth * 1.63 > 2500
-      ? this.setState({
-          distanceTop: 2500,
-        })
-      : this.setState({
-          distanceTop: window.innerWidth * 1.63,
-        })
-    console.log('Distance to top is ' + this.state.distanceTop)
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize)
-  }
-
-  render() {
-    return (
-      <div className={css(styles.projectWrapper)}>
-        <div className={css(styles.projectTitle)}>
-          <p>Spin Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Spin Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Spin Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Liberty Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Liberty Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Spin Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Spin Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <div className={css(styles.projectTitle)}>
-          <p>Spin Lamp, 2018</p>
-        </div>
-        <div className={css(styles.imageWrapper)}>
-          <img src={image_one} />
-        </div>
-        <StyledScrollTop
-          text=""
-          distance={this.state.distanceTop}
-          breakpoint={1024}
-          speed={500}
-          target={0}
-          icon={null}
-        />
+const Projects = () => {
+  return (
+    <div className={css(styles.projectWrapper)}>
+      <div className={css(styles.projectTitle)}>
+        <p>Spin Lamp, 2018</p>
       </div>
-    )
-  }
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Spin Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Spin Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Liberty Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Liberty Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Spin Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Spin Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <div className={css(styles.projectTitle)}>
+        <p>Spin Lamp, 2018</p>
+      </div>
+      <div className={css(styles.imageWrapper)}>
+        <img src={image_one} />
+      </div>
+      <StyledScrollTop
+        text=""
+        distance={1500}
+        breakpoint={1024}
+        speed={500}
+        target={0}
+        icon={null}
+      />
+    </div>
+  )
 }
 
 export default Projects
