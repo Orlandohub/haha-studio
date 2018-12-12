9ae19a85b0830b4cb1be73dd44b354962c209154
Alphascythian <42778608+Alphascythian@users.noreply.github.com>
2018-12-12 13:28:29 +0000

    first commit

--- src/layouts/styles.js
+++ src/layouts/styles.js
@@ -2,15 +2,25 @@ import facepaint from 'facepaint'
 const breakpoints = [576, 768, 1024, 1200]
 const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
 
-export const styles = {
-  pageWrapper: mq({
-    overflow: 'auto',
-    width: '100%',
-    height: 'auto',
-    display: 'block',
-    position: 'relative',
-    paddingLeft: ['32px', '32px', '80px', '135px', '160px'],
-    paddingRight: ['32px', '32px', '80px', '135px', '160px'],
-    paddingBottom: ['60px', '60px', '60px', '226px'],
-  }),
-}
+export const pageWrapper = mq({
+  overflow: 'auto',
+  width: '100%',
+  height: 'auto',
+  display: 'block',
+  position: 'relative',
+  paddingLeft: ['32px', '32px', '80px', '135px', '160px'],
+  paddingRight: ['32px', '32px', '80px', '135px', '160px'],
+  paddingBottom: ['60px', '60px', '60px', '226px'],
+})
+
+// ############### STYLING FOR ABOUT INDEX PAGE #################
+
+export const aboutWrapper = mq({
+  width: 993,
+  float: 'right',
+})
+export const aboutText = mq({})
+export const aboutImageWrapper = mq({})
+export const aboutImage = mq({})
+
+// ############### END OF STYLING FOR ABOUT INDEX PAGE #################
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from '../layouts'
-import { Grid, Row, Col } from 'react-bootstrap'
+import * as styles from '../layouts/styles'
+import image_one from '../../images/D_homepage_image_01.jpg'
 
 const About = ({ location }) => (
   <React.Fragment>
     <Layout location={location}>
-      <Grid fluid>
-        <Row>
-          <Col xs={12}>
-            <h2>About</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
-              nobis, molestias labore reprehenderit laborum ad necessitatibus
-              rem quam voluptatem temporibus amet similique tempore ullam cum
-              nulla et voluptatum! Eius, ab!
-            </p>
-          </Col>
-        </Row>
-      </Grid>
+      <div className={css(styles.abouttWrapper)}>
+        <div className={css(styles.aboutTextWrapper)}>
+          <p>
+            Laughter is instinctive, it can be understood by all of us,
+            regardless of the language we speak.
+          </p>
+          <p>
+            HAHA studio is the Stockholm based design practice founded by Arash
+            Eskafi and Yujin Chiang in 2015. Hailing from different cultures—
+            Scandinavia and East Asia— the studio is in constant search of what
+            can be understood by all of us. Like laughter, HAHA relies on
+            fundamental shapes, choice materials and universal truths to design
+            their iconic work. To create objects that all of us have an
+            instinctive response to, HAHA works hand in hand with craftspeople
+            in Sweden and Taiwan. The practice designs lighting fixtures,
+            accessories, limited editions, and undertakes regular collaborations
+            in interior architecture.
+          </p>
+          <p>
+            The studio’s work has been presented by Vitra, FRACAS Gallery,
+            Raumplan, Biennale Interieur and Stockholm Furniture Fair. They have
+            also showcased their work internationally in Brussels, Denmark,
+            London, Milan, New York, and Shanghai.
+          </p>
+        </div>
+        <div className={css(styles.aboutImageWrapper)} />
+        <img src={image_one} />
+      </div>
     </Layout>
   </React.Fragment>
 )
