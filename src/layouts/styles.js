4bc0b45020c775e9e4738ad0ac00613c4676aaa5
Alphascythian <42778608+Alphascythian@users.noreply.github.com>
2018-12-13 07:12:36 +0000

    About page
    done

--- src/layouts/styles.js
+++ src/layouts/styles.js
@@ -10,17 +10,42 @@ export const pageWrapper = mq({
   position: 'relative',
   paddingLeft: ['32px', '32px', '80px', '135px', '160px'],
   paddingRight: ['32px', '32px', '80px', '135px', '160px'],
-  paddingBottom: ['60px', '60px', '60px', '226px'],
+  //paddingBottom: ['60px', '60px', '60px', '226px'],
 })
 
 // ############### STYLING FOR ABOUT INDEX PAGE #################
 
 export const aboutWrapper = mq({
-  width: 993,
-  float: 'right',
+  width: '100%',
+  paddingTop: [15, 15, 15, 15, 22],
 })
-export const aboutText = mq({})
-export const aboutImageWrapper = mq({})
-export const aboutImage = mq({})
+export const aboutText = mq(aboutImageWrapper, {
+  fontSize: ['14px', '14px', '22px', '22px'],
+})
 
-// ############### END OF STYLING FOR ABOUT INDEX PAGE #################
+export const aboutImageWrapper = mq({
+  paddingTop: [40, 40, 40, 40, 53],
+  paddingBottom: [60, 60, 60, 60, 217],
+})
+
+export const leftFloatingEmptySpace = mq({
+  width: ['0%', '0%', '0%', '0%', '25%'],
+  float: 'left',
+  minHeight: [
+    '0px!important',
+    '0px!important',
+    '0px!important',
+    '0px!important',
+    '1px!important',
+  ],
+})
+export const styledAboutParagraph = mq({
+  margin: '0px!important',
+})
+export const aboutRightColumn = mq({
+  width: ['100%', '100%', '100%', '100%', '75%'],
+  float: 'left',
+  paddingLeft: [0, 0, 0, 0, 175],
+})
+
+// ###############  END  ##################
