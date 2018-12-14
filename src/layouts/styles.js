aeb0be01bb644455753dd70bcee782181a5a19aa
Alphascythian <42778608+Alphascythian@users.noreply.github.com>
2018-12-14 12:37:46 +0000

    ds
    sd

--- src/layouts/styles.js
+++ src/layouts/styles.js
@@ -2,13 +2,14 @@ import facepaint from 'facepaint'
 const breakpoints = [576, 768, 1024, 1200]
 const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
 
-export const pageWrapper = mq({
-  overflow: 'auto',
-  width: '100%',
-  height: 'auto',
-  display: 'block',
-  position: 'relative',
-  paddingLeft: ['32px', '32px', '80px', '135px', '160px'],
-  paddingRight: ['32px', '32px', '80px', '135px', '160px'],
-})
-//dffd
+export const styles = {
+  pageWrapper: mq({
+    overflow: 'auto',
+    width: '100%',
+    height: 'auto',
+    display: 'block',
+    position: 'relative',
+    paddingLeft: ['32px', '32px', '80px', '135px', '160px'],
+    paddingRight: ['32px', '32px', '80px', '135px', '160px'],
+  }),
+}
