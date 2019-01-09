60bd084c06504a458431db6b52e87db9c4b314d7
Orlando Goncalves <orlando.goncalves@gmail.com>
2019-01-09 15:23:11 +0100

    fixes #169

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,17 +30,45 @@ exports.createPages = ({ actions, graphql }) => {
 
     const posts = result.data.allMarkdownRemark.edges
 
+    const press = _.filter(posts, post => {
+      return post.node.frontmatter.templateKey === 'press-page'
+    })
+
+    _.forEach(press, (edge, index) => {
+      const id = edge.node.id
+      const next = index === 0
+        ? `${press[press.length - 1].node.fields.slug}`
+        : press[index - 1].node.fields.slug
+
+      const prev = press.length === index + 1
+        ? `${press[0].node.fields.slug}`
+        : press[index + 1].node.fields.slug
+
+      createPage({
+        path: edge.node.fields.slug,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+          prev,
+          next
+        },
+      })
+    })
+
     const texts = _.filter(posts, post => {
       return post.node.frontmatter.templateKey === 'text-page'
     })
 
     _.forEach(texts, (edge, index) => {
       const id = edge.node.id
-      const prev = index === 0
+      const next = index === 0
         ? `${texts[texts.length - 1].node.fields.slug}`
         : texts[index - 1].node.fields.slug
 
-      const next = texts.length === index + 1
+      const prev = texts.length === index + 1
         ? `${texts[0].node.fields.slug}`
         : texts[index + 1].node.fields.slug
 
@@ -60,6 +88,9 @@ exports.createPages = ({ actions, graphql }) => {
 
     posts.forEach(edge => {
       if (edge.node.frontmatter.templateKey === 'text-page') {
+        return null
+      }
+      if (edge.node.frontmatter.templateKey === 'press-page') {
         return null
       }
       const id = edge.node.id
