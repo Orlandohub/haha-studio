8813f6073398e66ebce25ef468e91c1475e8b781
Orlando Goncalves <orlando.goncalves@gmail.com>
2019-01-27 23:37:11 +0100

    back to the roots

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -139,9 +139,14 @@ export const cartShowBtn = mq({
   textAlign: 'center',
   backgroundColor: '#0A00FF',
   color: 'white',
-  borderRadius: 10,
+  borderRadius: 50,
   border: 'none',
   outline: 'none',
+  width: [12, 12, 22],
+  height: [12, 12, 22],
+  fontSize: [8, 8, 14],
+  lineHeight: 0,
+  padding: 2,
 })
 
 export const cartShowBtnHidden = mq(cartShowBtn, {
