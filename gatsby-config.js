56601aa24c64dd5af1f7376fb19fd5bb0645783a
Orlando Goncalves <orlando.goncalves@gmail.com>
2019-11-04 22:16:17 +0100

    google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,13 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: 'UA-151267808-1',
+      },
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
